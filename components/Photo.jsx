7c7eac3557b0607a254945c7c6b228116cacf7db
SAMADHAN DNYANESHWAR GHORPADE <136071800+SamadhanGh@users.noreply.github.com>
2025-01-23 17:39:09 +0530

    Update Photo.jsx

--- components/Photo.jsx
+++ components/Photo.jsx
@@ -19,7 +19,7 @@ const Photo = () => {
           className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] mix-blend-lighten absolute"
         >
           <Image
-            src="/public/assets/photo.png"
+            src="public/assets/photo.png"
             priority
             quality={100}
             fill
