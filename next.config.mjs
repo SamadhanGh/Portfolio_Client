6756077652377ebef55bf1f0b9a88b73e63d80cb
SAMADHAN DNYANESHWAR GHORPADE <136071800+SamadhanGh@users.noreply.github.com>
2025-01-23 15:20:20 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,20 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     images: {
+//         remotePatterns: [
+//             {
+//                 protocol: 'https',
+//                 hostname: 'i.ibb.co', // Replace with your domain(s)
+//                 pathname: '/**', // Match all paths
+//             },
+//         ],
+//     },
+// };
+
+// export default nextConfig;
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: 'export', // Enable static export
     images: {
         remotePatterns: [
             {
