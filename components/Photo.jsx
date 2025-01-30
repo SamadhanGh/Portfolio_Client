45995057d1719874abd3fbbd2cbd11739488012f
SAMADHAN DNYANESHWAR GHORPADE <136071800+SamadhanGh@users.noreply.github.com>
2025-01-30 09:37:14 +0530

    Update Photo.jsx

--- components/Photo.jsx
+++ components/Photo.jsx
@@ -17,7 +17,8 @@ const Photo = () => {
           className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] mix-blend-lighten absolute"
         >
           <Image
-            src="https://images.app.goo.gl/5LAJqGTBuBP9SwVT6" // Ensure this path is correct and accessible
+           // src="https://images.app.goo.gl/5LAJqGTBuBP9SwVT6" // Ensure this path is correct and accessible
+            src="public/assets/photo.png"
             alt="Descriptive text about the image"
             quality={100}
             width={498} // Explicitly set width and height for static images
